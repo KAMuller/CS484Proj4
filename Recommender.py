bbdbe75fb2e1c46887a32f606431fad879480456
Kalman Muller <kalexandremuller@gmail.com>
2019-04-27 01:23:10 -0400

    added code for user profiles and recommending scores

--- Recommender.py
+++ Recommender.py
@@ -1,49 +1,120 @@
+from sklearn.metrics.pairwise import cosine_similarity
+import numpy as np
 
-trainFile = open('C:/Users/kalex/PycharmProjects/CS484Proj4/Train.dat', 'r')
 trainList = []
-for line in trainFile:
-    splitLine = line.split(' ')
-    trainList.append(splitLine)
-print(trainList[1])
-actorFile = open('C:/Users/kalex/PycharmProjects/CS484Proj4/movie_actors.dat', 'r')
+with open('Train.dat', 'r') as trainFile:
+    for line in trainFile:
+        splitLine = line.split(' ')
+        trainList.append(splitLine)
+        trainList[-1][-1] = trainList[-1][-1].replace('\n', '')
+
+
 actorList = []
-for line in actorFile:
-    actSplitLine = line.split(' ')
-    actorList.append(actSplitLine)
-print(actorList[1])
-dirFile = open('C:/Users/kalex/PycharmProjects/CS484Proj4/movie_directors.dat', 'r')
+with open('movie_actors.dat', 'r') as actorFile:
+    for line in actorFile:
+        actSplitLine = line.split(' ')
+        actorList.append(actSplitLine)
+        actorList[-1][-1] = actorList[-1][-1].replace('\n', '')
+# print(actorList[1])
+
 dirList = []
-for line in dirFile:
-    dirSplitLine = line.split(' ')
-    dirList.append(dirSplitLine)
-print(dirList[1])
+with open('movie_directors.dat', 'r') as dirFile:
+    for line in dirFile:
+        dirSplitLine = line.split(' ')
+        dirList.append(dirSplitLine)
+        dirList[-1][-1] = dirList[-1][-1].replace('\n', '')
+# print(dirList[1])
+
 genresList = []
-with open('C:/Users/kalex/PycharmProjects/CS484Proj4/movie_genres.dat', 'r') as genFile:
+with open('movie_genres.dat', 'r') as genFile:
     for line in genFile:
         genSplitLine = line.split(' ')
         genresList.append(genSplitLine)
-print(genresList[1])
+        genresList[-1][-1] = genresList[-1][-1].replace('\n', '')
+# print(genresList[1])
+
 mTagsList = []
-with open('C:/Users/kalex/PycharmProjects/CS484Proj4/movie_tags.dat', 'r') as mTagsFile:
+with open('movie_tags.dat', 'r') as mTagsFile:
     for line in mTagsFile:
         mTagsSplitLine = line.split(' ')
         mTagsList.append(mTagsSplitLine)
-print(mTagsList[1])
+        mTagsList[-1][-1] = mTagsList[-1][-1].replace('\n', '')
+# print(mTagsList[1])
+
 tagsList = []
-with open('C:/Users/kalex/PycharmProjects/CS484Proj4/tags.dat', 'r') as tagsFile:
+with open('tags.dat', 'r') as tagsFile:
     for line in tagsFile:
         tagsSplitLine = line.split(' ')
         tagsList.append(tagsSplitLine)
-print(tagsList[1])
+        tagsList[-1][-1] = tagsList[-1][-1].replace('\n', '')
+# print(tagsList[1])
+
 useTList = []
-with open('C:/Users/kalex/PycharmProjects/CS484Proj4/user_taggedmovies.dat', 'r') as useTFile:
+with open('user_taggedmovies.dat', 'r') as useTFile:
     for line in useTFile:
         useTSplitLine = line.split(' ')
         useTList.append(useTSplitLine)
-print(useTList[1])
+        useTList[-1][-1] = useTList[-1][-1].replace('\n', '')
+# print(useTList[1])
+
 testList = []
-with open('C:/Users/kalex/PycharmProjects/CS484Proj4/test.dat', 'r') as testFile:
+with open('test.dat', 'r') as testFile:
     for line in testFile:
         testSplitLine = line.split(' ')
         testList.append(testSplitLine)
-print(testList[1])
+        testList[-1][-1] = testList[-1][-1].replace('\n', '')
+# print(testList[1])
+
+def uexists(profiles, uid):
+    for x in profiles:
+        if uid == x[0]:
+            return True
+    return False
+
+
+def getuidindex(profiles, uid):
+    for index,val in enumerate(profiles):
+        if uid == val[0]:
+            return index
+    return -1
+
+
+# placeholder vector
+trainList = trainList[1:-1]
+print(trainList[0][0])
+movieVect = [1, 1, 1, 1, 1, 1, 1, 1, 1]
+userProfiles = []
+for line in trainList:
+    if not uexists(userProfiles, line[0]):
+        profile = [line[0]]
+        proMovies = []
+        proMovieScores = []
+        profile.append(proMovies)
+        profile.append(proMovieScores)
+        userProfiles.append(profile)
+    index = getuidindex(userProfiles, line[0])
+    # get movie vector from id here, store vector in movieVect
+    userProfiles[index][1].append(movieVect)
+    userProfiles[index][2].append(line[2])
+print(userProfiles[0][0])
+print(userProfiles[0][1])
+print(len(userProfiles[0][1]))
+print(userProfiles[0][2])
+print(len(userProfiles[0][2]))
+
+k = 10
+outputScores = []
+for line in testList:
+    index = getuidindex(userProfiles, line[0])
+    # get movie vector and store to variable testMovVect
+    testMovVect = []
+    similarities = cosine_similarity(userProfiles[index], testMovVect)
+    sortO = (-similarities).argsort(axis=1)[:, :k]
+    weightTot = 0
+    weightSum = 0
+    for x in sortO:
+        weightTot += similarities[x]
+        weightSum = similarities[x] * userProfiles[index][2][x]
+    outputScores.append(weightSum/weightTot)
+
+

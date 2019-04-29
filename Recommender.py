58a49f355eda11b4ebf5da3a5917160660aa9bcd
Kalman Muller <kalexandremuller@gmail.com>
2019-04-28 20:26:51 -0400

    comments added

--- Recommender.py
+++ Recommender.py
@@ -1,9 +1,11 @@
+# Kalman Muller and Jonathan Barker
+# CS484 Assignment 4
 import os
 import sys
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 import numpy as np
-
+# load all information from files to list for use in the program
 trainList = []
 with open('Train.dat', 'r') as trainFile:
     for line in trainFile:
@@ -11,7 +13,6 @@ with open('Train.dat', 'r') as trainFile:
         trainList.append(splitLine)
         trainList[-1][-1] = trainList[-1][-1].replace('\n', '')
     trainList = trainList[1:-1]
-
 actorList = []
 with open('movie_actors.dat', 'r') as actorFile:
     for line in actorFile:
@@ -19,8 +20,6 @@ with open('movie_actors.dat', 'r') as actorFile:
         actorList.append(actSplitLine)
         actorList[-1][-1] = actorList[-1][-1].replace('\n', '')
     actorList = actorList[1:-1]
-# print(actorList[1])
-
 dirList = []
 with open('movie_directors.dat', 'r') as dirFile:
     for line in dirFile:
@@ -28,8 +27,6 @@ with open('movie_directors.dat', 'r') as dirFile:
         dirList.append(dirSplitLine)
         dirList[-1][-1] = dirList[-1][-1].replace('\n', '')
     dirList = dirList[1:-1]
-# print(dirList[1])
-
 genresList = []
 with open('movie_genres.dat', 'r') as genFile:
     for line in genFile:
@@ -37,8 +34,6 @@ with open('movie_genres.dat', 'r') as genFile:
         genresList.append(genSplitLine)
         genresList[-1][-1] = genresList[-1][-1].replace('\n', '')
     genresList = genresList[1:-1]
-# print(genresList[1])
-
 mTagsList = []
 with open('movie_tags.dat', 'r') as mTagsFile:
     for line in mTagsFile:
@@ -46,8 +41,6 @@ with open('movie_tags.dat', 'r') as mTagsFile:
         mTagsList.append(mTagsSplitLine)
         mTagsList[-1][-1] = mTagsList[-1][-1].replace('\n', '')
     mTagsList = mTagsList[1:-1]
-# print(mTagsList[1])
-
 tagsList = []
 with open('tags.dat', 'r') as tagsFile:
     for line in tagsFile:
@@ -55,16 +48,6 @@ with open('tags.dat', 'r') as tagsFile:
         tagsList.append(tagsSplitLine)
         tagsList[-1][-1] = tagsList[-1][-1].replace('\n', '')
     tagsList = tagsList[1:-1]
-# print(tagsList[1])
-
-useTList = []
-with open('user_taggedmovies.dat', 'r') as useTFile:
-    for line in useTFile:
-        useTSplitLine = line.split(' ')
-        useTList.append(useTSplitLine)
-        useTList[-1][-1] = useTList[-1][-1].replace('\n', '')
-# print(useTList[1])
-
 testList = []
 with open('test.dat', 'r') as testFile:
     for line in testFile:
@@ -72,7 +55,9 @@ with open('test.dat', 'r') as testFile:
         testList.append(testSplitLine)
         testList[-1][-1] = testList[-1][-1].replace('\n', '')
     testList = testList[1:-1]
-# print(testList[1])
+
+
+# used to get the string for the tag id value
 def getTagString(tagID):
     for i in tagsList:
         if tagID == i[0]:
@@ -80,6 +65,7 @@ def getTagString(tagID):
     return ''
 
 
+# used to check if a movie vector currently exist for the specified id
 def mVectExists(movVects, mID):
     for x in movVects:
         if mID == x[0]:
@@ -87,6 +73,7 @@ def mVectExists(movVects, mID):
     return False
 
 
+# used to check if a profile exist for the specified user id
 def uexists(profiles, uid):
     for x in profiles:
         if uid == x[0]:
@@ -94,12 +81,17 @@ def uexists(profiles, uid):
     return False
 
 
+# used to get the index for the specified user id in the profiles list
 def getuidindex(profiles, uid):
     for index,val in enumerate(profiles):
         if uid == val[0]:
             return index
     return -1
 
+# loops though the movie tags data list
+# creates profiles if it encounters a new user id
+# adds the tag string to the string for the profile
+# repeatedly adds the tag based on its weight value
 movList = []
 fullMovList = []
 movID = 0
@@ -112,7 +104,6 @@ for prog,x in enumerate(mTagsList):
         if firstCheck:
             fullMovList.append(string)
             string = ''
-            # movList[movInd - 1][1] = len(fullMovList) - 1
         movInd += 1
         movID = x[0]
         movie = [x[0]]
@@ -120,7 +111,6 @@ for prog,x in enumerate(mTagsList):
         movie.append(movVect)
         movList.append(movie)
         firstCheck = True
-#     get tag FROM X[1] AND tagsList
     tag = getTagString(x[1])
     string += " " + tag
     a = int(x[2])
@@ -133,6 +123,9 @@ for prog,x in enumerate(mTagsList):
     sys.stdout.flush()
 fullMovList.append(string)
 
+# loops though the movie director data list
+# creates profiles if it encounters a new user id
+# adds the director id string to the string for the profile
 movID = 0
 string = ''
 firstCheck = False
@@ -155,8 +148,11 @@ for prog, line in enumerate(dirList):
     progPerc = progPerc[0:4]
     sys.stdout.write("\r%s%s" % ('2/4 %', progPerc))
     sys.stdout.flush()
-##################################################
-# getting actors
+
+# loops though the movie actors data list
+# creates profiles if it encounters a new user id
+# adds the actor id string to the string for the profile
+# repeatedly adds the actor id based on its ranking value
 rank_hold = 0
 movID = 0
 string = ''
@@ -188,6 +184,9 @@ for prog, line in enumerate(actorList):
     sys.stdout.write("\r%s%s" % ('3/4 %', progPerc))
     sys.stdout.flush()
 
+# loops though the movie genres data list
+# creates profiles if it encounters a new user id
+# adds the genre string to the string for the profile
 movID = 0
 string = ''
 firstCheck = False
@@ -211,65 +210,29 @@ for prog, line in enumerate(genresList):
     sys.stdout.write("\r%s%s" % ('4/4 %', progPerc))
     sys.stdout.flush()
 
-
-# print('\rDone')
-# print("The first full movie list is now: ", fullMovList[0])
-
+# converts the strings for each movie to vectors using the sklearn TF-IDFVectorizer function
 vectorizer = TfidfVectorizer(max_features=400)
 vector = vectorizer.fit_transform(fullMovList)
 vector = vector.toarray()
 print('\rDone')
-# print(fullMovList[0])
-# for x in vector:
-#     print(len(x))
-#     print(x)
-# print(vector[0])
-# placeholder vector
+
+
+# gets the movie vector for the associated movie id
 def getVector(mID):
     for i in movList:
         if i[0] == mID:
             return vector[i[1]]
     return np.array([9, 9])
 
-
-# for x in testList:
-#     vect = getVector(x[1])
-#     print(vect.shape)
-#
-# for x in trainList:
-#     vect = getVector(x[1])
-#     print(vect.shape)
-
-# def checkMiss(mid, mList):
-#     for el in mList:
-#         if mid == el:
-#             return False
-#     return True
-#
-# missingMovies = []
-# for x in testList:
-#     vect = getVector(x[1])
-#     if np.array_equal(vect, np.array([9, 9])):
-#         if checkMiss(x[1], missingMovies):
-#             missingMovies.append(x[1])
-# print("test ", len(missingMovies))
-# print(missingMovies)
-#
-# tmissingMovies = []
-# for x in trainList:
-#     vect = getVector(x[1])
-#     if np.array_equal(vect, np.array([9, 9])):
-#         if checkMiss(x[1], tmissingMovies):
-#             tmissingMovies.append(x[1])
-# print("train ", len(tmissingMovies))
-# print(tmissingMovies)
+# generates the user taste profiles using the information from the train data list
+# each element in userProfiles stores the user id, the list of movie vectors for that user,
+# and the scores given by the user for each movie
 userProfiles = []
 print("Creating User Taste Profiles")
 curUser = 0
 index = 0
 for prog, line in enumerate(trainList):
     if curUser != line[0]:
-    # if not uexists(userProfiles, line[0]):
         curUser = line[0]
         profile = [line[0]]
         proMovies = []
@@ -278,71 +241,49 @@ for prog, line in enumerate(trainList):
         profile.append(proMovieScores)
         userProfiles.append(profile)
         index = getuidindex(userProfiles, line[0])
-    # get movie vector from id here, store vector in movieVect
     vect = getVector(line[1])
     if not np.array_equal(vect, np.array([9, 9])):
         userProfiles[index][1].append(getVector(line[1]))
         userProfiles[index][2].append(line[2])
-    # else:
-        # print("no tags for ", line[1])
     progPerc = str(prog/(len(trainList)))
     progPerc = progPerc[0:4]
     sys.stdout.write("\r%s%s" % ('%', progPerc))
     sys.stdout.flush()
 print("\rDone")
-# print("There are: ", len(userProfiles), "User Profiles")
-# movMin = 99999
-# movMax = 0
-# for x in userProfiles:
-#     if len(x[1]) < movMin:
-#         movMin = len(x[1])
-#     if len(x[1]) > movMin:
-#         movMax = len(x[1])
-# print(movMin)
-# print(movMax)
-# print("The first user is: ", userProfiles[0][0])
-# print("The movie vectors for profile 0 are: ", userProfiles[0][1], "Length: ", len(userProfiles[0][1]))
-# print("The movie scores for profile 0 are: ", userProfiles[0][2], "Length: ", len(userProfiles[0][2]))
-# exit()
+
+# for each line in test list
+# calculates the similarity of the specified movie to those in the users taste profile
+# predicts the users score for the movie based on a weighted average of the k most similar movies
 k = 15
 print("Recommending Scores")
 outputScores = []
-# testList = the data from test.dat
 fistCheck = False
 current_user = 0
 testMovVect = []
+indexCheck = []
 for prog, line in enumerate(testList):
     index = getuidindex(userProfiles, line[0])
-    # get movie vector and store to variable testMovVect
     testMovVect = getVector(line[1]).reshape(1, -1)
-    # print(index)
-    # print(len(testMovVect))
-    # print(len(userProfiles[index][1]))
-    # print(userProfiles[index][1])
     similarities = cosine_similarity(userProfiles[index][1], testMovVect)
-    # print(similarities)
     sortO = (-similarities).argsort(axis=0)[:k]
-    # print("sortO is: ", sortO)
-    # print(len(sortO))
     weightTot = 0
     weightSum = 0
     for a in range(len(sortO)):
         x = int(sortO[a])
-        # print("similarities[0][x] is: ", similarities[x])
-        # print("user profiles is: ", userProfiles[index][x])
         weightTot += float(similarities[x])
-        # print(weightTot)
         weightSum += float(similarities[x]) * float(userProfiles[index][2][x])
-        # print(weightSum)
     progPerc = str(prog / (len(testList)))
     progPerc = progPerc[0:4]
     sys.stdout.write("\r%s%s" % ('%', progPerc))
     sys.stdout.flush()
     if weightTot == 0:
+        indexCheck.append(line[1])
         outputScores.append(np.round(2.5, decimals=1))
     else:
+        indexCheck.append(line[1])
         outputScores.append(np.round(weightSum/weightTot, decimals=1))
 print("\rDone")
+# outputs scores to the file
 try:
     os.remove("output.dat")
 except OSError:
